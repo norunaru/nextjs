9a8514e592b1e0fdac0b2162495ac0d47cc015b3
gitlab 이름 <nwy98@naver.com>
2025-07-22 13:08:57 +0900

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,11 +6,7 @@ import LogoutBtn from "./LogoutBtn";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./lib/authOptions"; // authOptions 경로에 맞게 수정
 
-export default async function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default async function RootLayout({ children }) {
   const session = await getServerSession(authOptions); // ✅ await + authOptions 필요
 
   return (
